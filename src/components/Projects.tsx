6cf4838692a16d3c139d87725db20cb1e53a9329
Balam Venkata Sai Bharath <160164562+SaiBharath28@users.noreply.github.com>
2025-07-12 15:11:07 +0530

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -10,7 +10,7 @@ const Projects = () => {
         "A real-time chat platform with secure user authentication, group and private messaging, and live updates using Flask and Socket.IO.",
       technologies: ["Flask", "Socket.IO", "HTML", "CSS", "JavaScript"],
       github: "https://github.com/SaiBharath28/Chat-Appliaction",
-      live: "https://typing-speed-test-264nv4v3f-saibharath28s-projects.vercel.app/",
+      live: "https://chat-appliaction-1h63.vercel.app/login",
       featured: true
     },
     {
